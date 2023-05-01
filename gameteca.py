a50b2afa4a828f69b6e8a182715d68fd07b08e34
Kilder Augusto Da Silva Sousa <A0153149@redecorp.br>
2023-05-01 01:31:37 -0300

    Initial commit

---
+++ gameteca.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+class Game:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+#VARIAVEIS GLOBAIS = FATOR GLOBAL
+game1 = Game('Super Mario', 'Aventura', 'Nitendo')
+game2 = Game('Fzero', 'Corrida', 'Nitendo 64')
+game3 = Game('Mortal Kombat', 'Luta', 'Super Nitendo')
+lista_games = [game1, game2, game3]
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+
+    return render_template('lista.html', titulo='Gameteca - Nerdzinhos safados', games=lista_games)
+
+@app.route('/new')
+def new():
+    return render_template('new.html', titulo='New Game')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    games = Game(nome, categoria, console)
+    lista_games.append(games)
+    return render_template('lista.html', titulo='Gameteca - Nerdzinhos safados', games=lista_games)
+
+
+# trecho da app
+app.run(debug=True)
